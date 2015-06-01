3d06f4a58d5228e4b900614c2c44b02baaa49df0
GYF1 <guoyinfeng@live.com>
2015-06-01 22:12:23 +0800

    update

---
+++ src/pencil.js
@@ -0,0 +1,15 @@
+var Pencil = (function () {
+  'use strict';
+
+    function Pencil () {
+
+    }
+
+    return {
+      initPencil : function (domObj) {
+        new Pencil(domObj);
+      }
+    }
+
+
+}());
