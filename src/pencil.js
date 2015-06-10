4751ae4fbb4daee81c05bdf58afb2db78e3ae6b7
GYF1 <guoyinfeng@live.com>
2015-06-10 09:53:08 +0800

    backup

--- src/pencil.js
+++ src/pencil.js
@@ -1,15 +1,30 @@
 var Pencil = (function () {
   'use strict';
 
-    function Pencil () {
+  // Pencil Class
+  function Pencil() {
+    //new Pencil(domObj);
+  }
 
+  var initClickMode = function (domObj) {
+    var divs = document.getElementsByClassName(domObj.className);
+
+    for (var i = 0; i < divs.length; i++) {
+      divs[i].addEventListener('click', function (event) {
+        console.log('Hi!::', this.innerHTML);
+      });
     }
+  };
 
-    return {
-      initPencil : function (domObj) {
-        new Pencil(domObj);
+  return {
+    initPencil: function (domObj) {
+      switch (domObj.type) {
+        case 'click' :
+          initClickMode(domObj);
+          break;
       }
     }
+  }
 
 
 }());
